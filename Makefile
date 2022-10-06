43c597ae1362a847b3a99833900ed17a7170ae5d
kajikentaro <k.kentarou0412@gmail.com>
2022-10-06 07:36:15 +0000

    Add: フロントエンドのapiをgrpcを使って連帯

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
-OUT_DIR="./generated"
+OUT_DIR="./grpc_out"
 
 gen_grpc_front:
-	protoc \
-		--plugin="protoc-gen-ts=$(PROTOC_GEN_TS_PATH)" \
-		--js_out="import_style=commonjs,binary:$(OUT_DIR)" \
-		--ts_out="$(OUT_DIR)" \
-		helloworld.proto
+	cd frontend && rm -rf $(OUT_DIR)
+	cd frontend && mkdir $(OUT_DIR)
+	cd frontend && protoc \
+	--plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
+	--js_out="import_style=commonjs,binary:${OUT_DIR}" \
+	--ts_out="service=grpc-web:${OUT_DIR}" \
+	--proto_path=../grpc/ \
+	../grpc/*.proto
--- frontend/pages/api/hello.ts
+++ frontend/pages/api/hello.ts
@@ -1,13 +1,37 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { grpc } from "@improbable-eng/grpc-web";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { HelloReply, HelloRequest } from "../../grpc_out/grpc_pb";
+import { Greeter } from "../../grpc_out/grpc_pb_service";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const queryBooksRequest = new HelloRequest();
+  queryBooksRequest.setName("Geor");
+  grpc.invoke(Greeter.SayHello, {
+    request: queryBooksRequest,
+    host: "https://example.com",
+    onMessage: (message: HelloReply) => {
+      console.log("got book: ", message.toObject());
+    },
+    onEnd: (
+      code: grpc.Code,
+      msg: string | undefined,
+      trailers: grpc.Metadata
+    ) => {
+      if (code == grpc.Code.OK) {
+        console.log("all ok");
+      } else {
+        console.log("hit an error", code, msg, trailers);
+      }
+    },
+  });
+
+  res.status(200).json({ name: "John Doe" });
 }
